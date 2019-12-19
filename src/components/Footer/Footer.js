82511445ab50e01ff15c668135e6355c140c1d1b
jujopico <sujunqt@gmail.com>
2019-12-18 19:45:11 -0500

    add copyright, links and download button

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Footer.css'
+import githubLogo from '../../assets/images/githubLogo.png'
+import linkedinIcon from '../../assets/images/linkedinIcon.png'
+import JunSuResume from '../../assets/pdf/JunSuResume.pdf'
+
+const Footer = () => (
+  <footer>
+    <div className="copyRight">&copy;2019 Made with ❤ by Jun Su</div>
+    <div className="socialMedia">
+      <div className="logos">
+        <a href="https://github.com/jujopico" target="blank">
+          <img src={githubLogo} alt="github" />
+        </a>
+        <a href="https://www.linkedin.com/in/junsu1112/" target="blank">
+          <img src={linkedinIcon} alt="linkedin"/>
+        </a>
+      </div>
+      <div className="resume">
+        <a target="blank" download href={JunSuResume}>
+          <button>Download Resume</button>
+        </a>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer
