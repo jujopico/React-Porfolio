7227b99147a4c0fe538ba1988960f6e60199c505
jujopico <sujunqt@gmail.com>
2019-12-19 00:46:00 -0500

    fix navbar underline and the best of dec 18

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Carousel from './Carousel/Carousel'
-import Floating from './Floating/Floating'
 
 class Home extends React.Component {
   render(){
     return(
       <>
       <Carousel />
-      <Floating />
       </>
     )
   }
