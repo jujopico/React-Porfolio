e9ecb8fc226f36ffe77bd61abb266450e775b527
jujopico <sujunqt@gmail.com>
2019-12-18 19:44:24 -0500

    selfie and self intro

---
+++ src/components/Selfie/Selfie.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Selfie.css'
+
+const Selfie = () => (
+  <div>
+    <div className="selfie">
+      <img src="https://wynstore-assets.herokuapp.com/images/Artboard_2.png" alt="junsu"/>
+    </div>
+    <div className="name">Jun Su</div>
+    <div className="self-intro">WEB DEVELOPER IN MIAMI, FL</div>
+  </div>
+)
+
+export default Selfie
