1969da9f9de35507d243f2a810122781beae357f
jujopico <sujunqt@gmail.com>
2019-12-19 19:40:47 -0500

    add animation to words on carousel and finish home page

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -9,6 +9,14 @@ const Carousel= () => {
       "https://66.media.tumblr.com/ec7bbd21ed673f093d64482609e0dc39/b046c728e84f4d9f-8a/s1280x1920/a6ab15cfae38c11c22f46990471cc77b6c9c896f.jpg", 
       "https://66.media.tumblr.com/04c973478f69bb3bc7940e427693b2cb/b046c728e84f4d9f-b0/s1280x1920/4f6b8f991f9ecf206d557b638511f070f90789f4.jpg"
     ]
+    const wordsJun = " Jun"
+    const splitJun = wordsJun.split("")
+    const wordsDeveloper = " Developer"
+    const splitDeveoper = wordsDeveloper.split("")
+    const wordsCodes = " Clean Codes"
+    const splitCodes = wordsCodes.split("")
+    const wordsToys = " Toys"
+    const splitToys = wordsToys.split("")
 
     React.useEffect(() => {
       const interval = setInterval(() => {
@@ -36,15 +44,43 @@ const Carousel= () => {
         </Gallery>
       </div>
         <div className="carouselInfo">
-          Hi!
-          <br />
-          I'm Jun,
-          <br />
-          a Web Developer.
+          <div>
+            Hi!
+          </div>
+          <div className="junContainer">
+            I'm
+            {
+              splitJun.map((letter, i) => (
+                <div className="jun" key={i} >{letter === " " ? "\xa0" : letter}</div>
+              ))
+            },
+          </div>
+          <div className="developerContainer">
+            a web
+            {
+              splitDeveoper.map((letter, i) => (
+                <div className="developer" key={i} >{letter === " " ? "\xa0" : letter}</div>
+              ))
+            }.
+          </div>
+          <div className="codesContainer">
+            I love
+            {
+              splitCodes.map((letter, i) => (
+                <div className="codes" key={i} >{letter === " " ? "\xa0" : letter}</div>
+              ))
+            }
+          </div>
+          <div className="toysContainer">
+            &
+            {
+              splitToys.map((letter, i) => (
+                <div className="toys" key={i} >{letter === " " ? "\xa0" : letter}</div>
+              ))
+            }.
+          </div>
         </div>
       </div>
-
-
     )
   }
 
