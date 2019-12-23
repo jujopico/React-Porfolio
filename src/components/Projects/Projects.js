a05f889f45e38813c8d1eacb3e801f218e0eccd7
jujopico <sujunqt@gmail.com>
2019-12-23 18:42:17 -0500

    add Contact and Projects

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Projects extends React.Component {
+  render(){
+    return(
+      <div className="projectsList">
+        
+
+      </div> 
+    )
+  }
+}
+
+export default Projects
