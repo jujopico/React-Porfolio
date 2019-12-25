b18150b59de10294aae70767ea33cd629d1d2f0b
jujopico <sujunqt@gmail.com>
2019-12-24 23:46:16 -0500

    try to navigate to different sections on the same page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,65 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { withRouter } from "react-router";
+import { Link, animateScroll as scroll } from "react-scroll"
 import './Navbar.css'
 import Selfie from '../Selfie/Selfie'
 import Footer from '../Footer/Footer'
 
-
-const Navbar = props => {
-  return(
-    <div className="navbar">
-      <Selfie />
-      <nav>
-        <NavLink className={props.location.pathname === "/" ? "active" : "inactive"} exact to="/">
-          HOME
-        </NavLink>
-        <NavLink className={props.location.pathname === "/projects" ? "active" : "inactive"} to="/projects">
-          PROJECTS
-          </NavLink>
-        <NavLink className={props.location.pathname === "/contact" ? "active" : "inactive"} to="/contact">
-          CONTACT
-        </NavLink>
-      </nav>
-      <Footer />
-    </div>
-  )
+class Navbar extends React.Component {
+  scrollToTop = () => {
+    scroll.scrollToTop()
+  }
+  render() {
+    return (
+      <div className="navbar">
+        <Selfie />
+        <nav>
+          <div>
+          <ul>
+          <li className="nav-item">
+            <Link
+              activeClass="active"
+              to=""
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={this.scrollToTop}
+            >
+              HOME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section1"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                
+              >
+                PROJECTS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section2"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                CONTACT
+              </Link>
+            </li>
+          </ul>
+          </div>
+        </nav>
+        <Footer />
+      </div>
+    )
+  }
 }
 
-export default withRouter(Navbar)
+export default Navbar
