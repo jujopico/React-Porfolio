e91c18b2d22087b1b47d7bbb878532f556ece4ff
jujopico <sujunqt@gmail.com>
2019-12-24 23:47:19 -0500

    divide different sections on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Carousel from './Carousel/Carousel'
+import Projects from './Projects/Projects'
+import Contact from './Contact/Contact'
+import Section from '../Section'
 
 class Home extends React.Component {
   render(){
     return(
-      <>
-      <Carousel />
-      </>
+      <div className="mainContainer">
+        <Carousel />
+        <div className="projectsContainer">
+          <Section
+              title="PROJECTS"
+              id="section1"
+            />
+          <Projects />
+        </div>
+        <div className="contactContainer">
+        <Section
+            title="CONTACT"
+            id="section2"
+          />
+        <Contact />
+        </div>
+      </div>
     )
   }
 }
