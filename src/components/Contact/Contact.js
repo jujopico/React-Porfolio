ee600b212eb36be21922453b32b6f6c92a654cbf
jujopico <sujunqt@gmail.com>
2019-12-26 16:28:26 -0500

    add contact section and funtion

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import './Contact.css'
 
-class Contact extends React.Component {
-  render(){
-    return(
-      "I'm the contact!!!!!!"
-    )
-  }
-}
+const Contact = () => (
+  <div className="contactContainer">
+    <div className="contactTitle">
+      Thank you for stopping by. I will get back to you ASAP! 
+    </div>
+    <form className="contactForm" action="https://formspree.io/sujunqt@gmail.com" method="POST" >
+    <div className="contactInfo">
+      <div className="contactName">
+        <input type="text" name="name" autoComplete="off" placeholder="NAME" required />
+      </div>
+      <div className="contactEmail">
+        <input type="email" name="email" autoComplete="off" placeholder="EMAIL" required />
+      </div>
+      <div className="contactPhone">
+        <input type="phone" name="phone" autoComplete="off" placeholder="PHONE NUMBER" />
+      </div>
+    </div>
+    <div className="contactText">
+      <textarea name="message" placeholder="MESSAGE" rows="10" cols="100" required />
+    </div>
+    <button className="contactButton" type="submit">Submit</button>
+    </form>
+  </div>
+)
+
 export default Contact
