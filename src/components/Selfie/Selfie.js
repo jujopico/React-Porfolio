48e84fc748c82ff5e0dda444c93e7873e95e7aab
jujopico <sujunqt@gmail.com>
2019-12-31 16:42:50 -0500

    make navbar responsive

--- src/components/Selfie/Selfie.js
+++ src/components/Selfie/Selfie.js
@@ -3,9 +3,7 @@ import './Selfie.css'
 
 const Selfie = () => (
   <div className="selfieContainer">
-    <div className="selfie">
-      <img src="https://wynstore-assets.herokuapp.com/images/Artboard_2.png" alt="junsu"/>
-    </div>
+    <img src="https://wynstore-assets.herokuapp.com/images/Artboard_2.png" alt="junsu"/>
     <div className="name">Jun Su</div>
     <div className="self-intro">WEB DEVELOPER IN MIAMI, FL</div>
   </div>
