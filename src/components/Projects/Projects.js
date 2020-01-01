e2c8540fd2613abecbcfceb1f3ca96cd5e2da03f
jujopico <sujunqt@gmail.com>
2020-01-01 15:00:43 -0500

    change projects page desgin and css

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,37 +15,58 @@ class Projects extends React.Component {
           <div className="videojuegoContainer">
             <div className="heroImg">
               <img src={ videojuego } alt="videojueGO!" className="projectsImg" />
-              <a href="https://github.com/jujopico/videojueGO" className="githubLinks">Check on Github</a>
+              <div className="overlay">
+                <div className="projectsDescription">
+                  A video game search engine based on RAWG games database <span className="highlightsInDesp">API</span>, <span className="highlightsInDesp">Axios</span> and <span className="highlightsInDesp">React.js</span>, which allows users to look up detailed game information.
+                </div>
+              </div>
             </div>
             <div className="projectsTitle">
-              videojueGO!
+              videojueGO
             </div>
-            <div className="projectsDescription">
-              A video game search engine based on RAWG games database <span className="highlightsInDesp">API</span>, <span className="highlightsInDesp">Axios</span> and <span className="highlightsInDesp">React.js</span>, which allows users to look up detailed game information.
+            <div className="links">
+              <ul>
+                <li><a href="https://github.com/jujopico/videojueGO">➨ Check on Github</a></li>
+                <li><a href="https://videojuego.herokuapp.com/">➨ Check on Webstie</a></li>
+              </ul>
             </div>
           </div>
           <div className="wynstoreContainer">
             <div className="heroImg">
               <img src={ wynstore } alt="wynstore" className="projectsImg" />
-              <a href="https://github.com/jujopico/wynstore" className="githubLinks">Check on Github</a>
+              <div className="overlay">
+                <div className="projectsDescription">
+                  An e-commerce site mostly based on <span className="highlightsInDesp">Ruby</span>, <span className="highlightsInDesp">Ruby on Rails</span>, <span className="highlightsInDesp">React.js</span> and <span className="highlightsInDesp">API</span> for signed-up/guest account to purchase Wyncode Academy merchandises and admin account to manager orders.
+                </div>
+              </div>
             </div>
             <div className="projectsTitle">
               Wynstore
             </div>
-            <div className="projectsDescription">
-              An e-commerce site mostly based on <span className="highlightsInDesp">Ruby</span>, <span className="highlightsInDesp">Ruby on Rails</span>, <span className="highlightsInDesp">React.js</span> and <span className="highlightsInDesp">API</span> for signed-up/guest account to purchase Wyncode Academy merchandises and admin account to manager orders.
+            <div className="links">
+              <ul>
+                <li><a href="https://github.com/jujopico/wynstore">➨ Check on Github</a></li>
+                <li><a href="https://wynstore.herokuapp.com/">➨ Check on Webstie</a></li>
+              </ul>
             </div>
           </div>
           <div className="portfolioContainer">
             <div className="heroImg">
               <img src={ portfolio } alt="portfolio" className="projectsImg" />
-              <a href="https://github.com/jujopico/React-Porfolio" className="githubLinks">Check on Github</a>
+              <div className="overlay">
+                <div className="projectsDescription">
+                  A portfolio based on <span className="highlightsInDesp">React</span> and <span className="highlightsInDesp">Javascript</span>, which allows visitors to navigate to different pages in/out of this site and download resume.
+                </div>
+              </div>
             </div>
             <div className="projectsTitle">
               Portfolio
             </div>
-            <div className="projectsDescription">
-              A portfolio based on <span className="highlightsInDesp">React</span> and <span className="highlightsInDesp">Javascript</span>, which allows visitors to navigate to different pages in/out of this site and download resume.
+            <div className="links">
+              <ul>
+                <li><a href="https://github.com/jujopico/React-Porfolio">➨ Check on Github</a></li>
+                <li><a href="https://www.junsu.dev/">➨ Check on Webstie</a></li>
+              </ul>
             </div>
           </div>  
         </div>
